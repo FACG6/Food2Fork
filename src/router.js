3352c05e1b60510675b49a14b8fb43859c8d1057
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-06 09:12:48 +0200

    edit

--- src/router.js
+++ src/router.js
@@ -5,7 +5,8 @@ const router = (request, response) => {
         handelHomePage(request, response);
     } else if (endpoint.indexOf('/public') !== -1) {
         handelPublicPage(request, response);
-    } else if (endpoint === '/food') {
+    } else if (endpoint === '/food'&& request.method==='POST') {
+         console.log(endpoint);
         handelHttpRequest(request, response);
     } else {
         response.writeHead(404, { 'content-Type': 'text/html' });
