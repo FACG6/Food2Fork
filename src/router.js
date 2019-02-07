c192d3d23423cf842c41d3dfdb1a92fea3395bec
Jamalat-shamallakh <jamalatshamallakh@gmail.com>
2019-02-07 09:45:13 +0200

    solve problem from eslint

--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
-const { handelHomePage, handelPublicPage, handelHttpRequest } = require('./handler')
+const { handelHomePage, handelPublicPage, handelHttpRequest } = require('./handler');
+
 const router = (request, response) => {
-    let endpoint = request.url;
-    if (endpoint === '/') {
-        handelHomePage(request, response);
-    } else if (endpoint.indexOf('/public') !== -1) {
-        handelPublicPage(request, response);
-    } else if (endpoint === '/food'&& request.method==='POST') {
-         console.log(endpoint);
-        handelHttpRequest(request, response);
-    } else {
-        response.writeHead(404, { 'content-Type': 'text/html' });
-        response.end('<h2>Server error</h2>');
-    }
-}
+  const endpoint = request.url;
+  if (endpoint === '/') {
+    handelHomePage(request, response);
+  } else if (endpoint.indexOf('/public') !== -1) {
+    handelPublicPage(request, response);
+  } else if (endpoint === '/food' && request.method === 'POST') {
+    handelHttpRequest(request, response);
+  } else {
+    response.writeHead(404, { 'content-Type': 'text/html' });
+    response.end('<h2>Server error</h2>');
+  }
+};
 module.exports = router;
